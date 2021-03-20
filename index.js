7ed4ff1d1a83f37af7c6004b59f6821699237fd8
zevk9191 <12zahar34@gmail.com>
2021-03-20 13:25:10 +0200

    feat: add new function

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-const METER = '';
-const KILOMETER = '';
-const MILE = '';
-const INCH = '';
+const METER = '1';
+const KILOMETER = '0.001';
+const MILE = '0.0006';
+const INCH = '39';
+
+let number = prompt('Напишіть число яке хочете перевести', '');
+
+alert(`Метри: ${+METER * +number}`);
+alert(`Кілометри: ${+KILOMETER * +number}`);
+alert(`Милі: ${+MILE * +number}`);
+alert(`Дюйми: ${+INCH * +number}`);
+
+
+
+
 
