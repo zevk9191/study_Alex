900f8bafe5a8f6dc63aea5b2a6d57239f2fe38ec
zevk9191 <12zahar34@gmail.com>
2021-03-20 14:08:13 +0200

    fix: change new mistakes

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const KILOMETER = 0.001;
 const MILE = 0.0006;
 const INCH = 39;
 
-let number = prompt('Напишіть число яке хочете перевести', );
+const number = +prompt('Напишіть число яке хочете перевести', '');
 
-alert(`Метри: ${+METER * +number}
-    Кілометри: ${+KILOMETER * +number}
-    Милі: ${+MILE * +number}
-    Дюйми: ${+INCH * +number}`
+alert(`Метри: ${METER * number}
+    Кілометри: ${KILOMETER * number}
+    Милі: ${MILE * number}
+    Дюйми: ${INCH * number}`
 );
