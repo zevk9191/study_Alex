f45b94301704e96995bd142a420920a36a60eade
Zakhar Bihun <12zahar34@gmail.com>
2021-09-10 17:28:10 +0300

    refactor(script.js, index.html, style-bottums.css) change formatting;

--- js/script.js
+++ js/script.js
@@ -1,13 +1,13 @@
 function toMeter(numberIn, nameOfSize) {
   switch (nameOfSize) {
     case 'MILE':
-      return (numberIn *= 1609.34);
+      return numberIn * 1609.34;
     case 'KILOMETER':
-      return (numberIn *= 1000);
+      return numberIn * 1000;
     case 'CENTIMETER':
-      return (numberIn *= 0.01);
+      return numberIn * 0.01;
     case 'MILIMETER':
-      return (numberIn *= 0.001);
+      return numberIn * 0.001;
     case 'METER':
       return numberIn;
     default:
@@ -18,13 +18,13 @@ function toMeter(numberIn, nameOfSize) {
 function fromMeter(result, nameOfSizeOut) {
   switch (nameOfSizeOut) {
     case 'MILE':
-      return (result *= 0.000621371);
+      return result * 0.000621371;
     case 'KILOMETER':
-      return (result *= 0.001);
+      return result * 0.001;
     case 'CENTIMETER':
-      return (result *= 100);
+      return result * 100;
     case 'MILIMETER':
-      return (result *= 1000);
+      return result * 1000;
     case 'METER':
       return result;
     default:
